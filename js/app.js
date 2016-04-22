6c216b648b03e99ad15cc8c1d001be75f91aa88b
Clay Griswold <ccgriswold@gmail.com>
2016-04-22 10:09:24 -0400

    Got the initial character list to load, PROGRESS! , now on to detailed view work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 //jslint esnext: true
 //jshint esnext: true
 
-var mainApp = angular.module('HeroSearchApp', [ngRoute]);
-var mainRequestUrl = 'http://gateway.marvel.com:80/v1/public/characters?events=29&orderBy=name&apikey=7ac2434b13b9586847ea8973d001c6a6&limit=10';
-//var mainRequestUrl = 'http://gateway.marvel.com:80/v1/public/characters?orderBy=name&apikey=7ac2434b13b9586847ea8973d001c6a6&limit=50';
-var secondRequestUrl = 'https://gateway.marvel.com:443/v1/public/characters/'+hero.id+'/events?apikey=7ac2434b13b9586847ea8973d001c6a6';
-
+let mainApp = angular.module('HeroSearchApp', ['ngRoute']);
+let mainRequestUrl = 'http://gateway.marvel.com:80/v1/public/characters?events=29&apikey=7ac2434b13b9586847ea8973d001c6a6&limit=10';
 
 mainApp.config(['$routeProvider', function($routeProvider){
   $routeProvider
@@ -16,39 +13,71 @@ mainApp.config(['$routeProvider', function($routeProvider){
     .when('/details', {
       controller: 'DetailsController',
       templateUrl: 'optionviews/extradetailed.html',
-    });
+    })/*.otherwise({
+      controller: 'HeroListController',
+      templateUrl: 'optionviews/herolist.html',
+    })*/;
 }]);
 
-mainApp.controller('HeroListController', ['$scope', '$http', '$route', 'HeroList', function($scope, $http, $route, HeroList){
-  $scope.heroes = [];
-  $scope.events=[];
-  $scope.herosearch = '';
-  $scope.currentchoice = {
-    name: '',
-    id: '',
-  };
+//Mock controller
+// mainApp.controller('HeroListController', function($scope){
+//   console.log('The List of Heroes');
+// });
+//mainApp.controller('DetailsController', function($scope){
+//   console.log('The Extra Details of Clicked Hero');
+// });
+
+mainApp.controller('HeroListController', ['$scope', '$http', 'HeroDetails', function($scope, $http, HeroDetails){
 
   $http({
-    method: 'get',
+    method: 'GET',
     url: mainRequestUrl,
   }).then(function (response){
-    console.log(response.data.data.results);
-    heroes = response.data.data.results;
+    console.log('first response', response.data.data.results);
+    $scope.heroes = response.data.data.results;
   });
-  
+    $scope.extradetails = function(hero){
+      HeroDetails.extradetails(hero);
+    };
+
 }]);
 
-mainApp.controller('DetailsController', ['$scope', '$http', 'extradetails', function($scope, $http, extradetails){
-
+mainApp.controller('DetailsController', ['$scope', '$http', 'HeroDetails', function($scope, $http, HeroDetails){
+    // let chardets = HeroDetails.extradetails(hero);
+    $scope.name = hero.name;
+    $scope.description = hero.description;
     $scope.clickedHeroPic = hero.thumbnail.path+'.'+hero.thumbnail.extension;
-    console.log($scope.clickedHeroPic);
-    $scope.currentchoice.name = hero.name;
+  }]);
+
+mainApp.factory('HeroDetails', ['$http', function ($http) {
+  return{
+  extradetails: function(hero){
+    var secondRequestUrl = 'https://gateway.marvel.com:443/v1/public/characters/'+hero.id+'/events?apikey=7ac2434b13b9586847ea8973d001c6a6';
     $http({
-      method: 'get',
+      method: 'GET',
       url: secondRequestUrl,
     }).then(function (response){
       $scope.events = response.data.data.results;
       console.log(response.data.data.results);
 
     });
+  },
+};
 }]);
+
+
+//
+//     $scope.events=[];
+//
+//     $scope.clickedHeroPic = hero.thumbnail.path+'.'+hero.thumbnail.extension;
+//     console.log($scope.clickedHeroPic);
+//     $scope.currentchoice.name = hero.name;
+//
+// }]);
+//
+// $scope.heroes = [];
+// $scope.herosearch = '';
+// $scope.currentchoice = {
+//   name: '',
+//   id: '',
+// };
