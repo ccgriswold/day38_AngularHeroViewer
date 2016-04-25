dac05978a6f60925b7ba9526eb0a1ec831d51108
Clay Griswold <ccgriswold@gmail.com>
2016-04-25 01:25:59 -0400

    No where close to a complete project. I can't get past detailed view

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 //jshint esnext: true
 
 let mainApp = angular.module('HeroSearchApp', ['ngRoute']);
-let mainRequestUrl = 'http://gateway.marvel.com:80/v1/public/characters?events=29&apikey=7ac2434b13b9586847ea8973d001c6a6&limit=10';
 
 mainApp.config(['$routeProvider', function($routeProvider){
   $routeProvider
@@ -13,10 +12,15 @@ mainApp.config(['$routeProvider', function($routeProvider){
     .when('/details', {
       controller: 'DetailsController',
       templateUrl: 'optionviews/extradetailed.html',
-    })/*.otherwise({
-      controller: 'HeroListController',
-      templateUrl: 'optionviews/herolist.html',
-    })*/;
+    })
+    .when('/event', {
+      controller: 'EventViewController',
+      templateUrl: 'sections/event-view.html',
+    })
+    .otherwise({
+      controller: 'ListViewController',
+      templateUrl: 'sections/list-view.html',
+        });
 }]);
 
 //Mock controller
@@ -27,43 +31,83 @@ mainApp.config(['$routeProvider', function($routeProvider){
 //   console.log('The Extra Details of Clicked Hero');
 // });
 
-mainApp.controller('HeroListController', ['$scope', '$http', 'HeroDetails', function($scope, $http, HeroDetails){
+mainApp.controller('HeroListController', ['$scope', '$http', 'HeroSelected', function($scope, $http, HeroSelected){
+    $scope.heroes = HeroSelected.grabHeroes();
+
+    $scope.extradetails = function(hero){
+      HeroSelected.extradetails(hero);
+    };
+    $scope.heroSearch = hero.name;
+}]);
+
+mainApp.controller('DetailsController', ['$scope', '$http', 'HeroSelected','$routeParams', function($scope, $http, $routeParams, HeroSelected){
+    $scope.events = HeroSelected.heroEvent();
+    $scope.currentChoice = HeroSelected.getSelected();
+}]);
+
+mainApp.controller('EventsController', ['$scope', '$http', '$routeParams', 'HeroSelected', function($scope, $http, $routeParams, HeroSelected){
+  $scope.eventID = $routeParams.heroEvents;
+  HeroSelected.setEventId($routeParams.heroEvents);
+  HeroSelected.grabEventName($http);
+  HeroSelected.grabEventDetails($http);
+  $scope.heroPerson = HeroSelected.heroEvent();
+  $scope.eventName = HeroSelected.setEventName();
+
+  $scope.nextHero = function(heroPerson){
+    HeroSelected.currentHero(heroPerson);
+  };
+}]);
+
+mainApp.factory('HeroSelected', function ($http) {
+  let name = '';
+  var heroes = [];
+  let events = [];
+  let currentChoice = {
+    name: '',
+    pic: '',
+    id: '',
+  };
 
   $http({
     method: 'GET',
-    url: mainRequestUrl,
+    url: 'http://gateway.marvel.com:80/v1/public/characters?events=29&apikey=7ac2434b13b9586847ea8973d001c6a6&limit=100',
   }).then(function (response){
     console.log('first response', response.data.data.results);
-    $scope.heroes = response.data.data.results;
+    return response.data.data.results;
   });
-    $scope.extradetails = function(hero){
-      HeroDetails.extradetails(hero);
-    };
 
-}]);
-
-mainApp.controller('DetailsController', ['$scope', '$http', 'HeroDetails', function($scope, $http, HeroDetails){
-    // let chardets = HeroDetails.extradetails(hero);
-    $scope.name = hero.name;
-    $scope.description = hero.description;
-    $scope.clickedHeroPic = hero.thumbnail.path+'.'+hero.thumbnail.extension;
-  }]);
-
-mainApp.factory('HeroDetails', ['$http', function ($http) {
   return{
-  extradetails: function(hero){
-    var secondRequestUrl = 'https://gateway.marvel.com:443/v1/public/characters/'+hero.id+'/events?apikey=7ac2434b13b9586847ea8973d001c6a6';
-    $http({
-      method: 'GET',
-      url: secondRequestUrl,
-    }).then(function (response){
-      $scope.events = response.data.data.results;
-      console.log(response.data.data.results);
+    grabHeroes: function(){
+      return heroes;
+    },
+    getSelected: function(){
+      return currentChoice;
+    },
+    heroEvent: function(){
+      return events;
+    },
 
-    });
-  },
-};
-}]);
+    currentHero: function(hero){
+      // console.log('currentHero is selected');
+      currentChoice.name = hero.name;
+      currentChoice.pic = (hero.thumbnail.path + hero.thumbnail.extension);
+      currentChoice.id = hero.id;
+    },
+
+    extradetails: function($http){
+      $http({
+        method: 'GET',
+        url: 'https://gateway.marvel.com:443/v1/public/characters/'+hero.id+'/events?apikey=7ac2434b13b9586847ea8973d001c6a6',
+      }).then(function (response){
+        // $scope.events = response.data.data.results;
+        console.log(response.data.data.results);
+      });
+    },
+    setEventName: function(){
+      return 
+    }
+  };
+});
 
 
 //
